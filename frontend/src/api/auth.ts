d7ebde78217f8ee07e4f089ddd6d57ea0b706513
joams mogro gomez <joamsmg@gmail.com>
2024-10-13 19:20:53 -0400

    feat: some text removed

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,8 +1,6 @@
 import { LoginUser, RegisterUser, Token } from "../types/authInterfaces";
 import axios from "./axios";
 
-//const API = "https://nc-c21-26-n-node-react.onrender.com/api/users";
-
 export const registerRequest = (user: RegisterUser) =>
   axios.post(`/register`, user);
 
