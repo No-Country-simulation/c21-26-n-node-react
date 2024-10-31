50289749b5a70fcd3f1107d7cd0dd960d1f659c5
illiCristian <svrk73@gmail.com>
2024-10-30 21:52:35 -0300

    Merge branch 'backend' of https://github.com/No-Country-simulation/c21-26-n-node-react into backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import { AppRoutes } from "./AppRoutes"
+import { AppRoutes } from "./AppRoutes";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-      <AppRoutes />
+      <div className="min-h-screen bg-background">
+        <Navbar />
+        <main className="pb-16 md:pb-0 md:pt-16">
+          <AppRoutes />
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
